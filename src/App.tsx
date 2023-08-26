c8f48e89ddabf5e73a759671257310ab8b7ed1d6
Aleksandr_k <rzn.russia4912@gmail.com>
2023-08-26 22:51:47 +0300

    styled components Btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { styled } from 'styled-components';
 
 function App() {
   return (
     <div className="App">
-      Proj-styled-Components
+      <Box>
+        <StyledBtn>Hello</StyledBtn>
+        <SuperButton>SuperButton</SuperButton>
+      </Box>
     </div>
   );
 }
 
 export default App;
+
+const StyledBtn = styled.button`
+  border: none;
+  background-color: #fb3f78;
+  padding: 19px 20px;
+  border-radius: 5px;
+  color: snow;
+  font-size: 2rem;
+  font-weight: bold;
+`
+
+const SuperButton = styled(StyledBtn)`
+  border-radius: 5px;
+  background-color: #ffe869;
+  color: black;
+`
+
+const Box = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
