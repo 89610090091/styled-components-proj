5b23c4b9748f28fb670622e938b30d8c3cae2cfb
Aleksandr_k <rzn.russia4912@gmail.com>
2023-08-27 20:41:00 +0300

    styledComponentsStyleButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import './App.css';
 import { styled } from 'styled-components';
-import { hover } from '@testing-library/user-event/dist/hover';
+// import { hover } from '@testing-library/user-event/dist/hover';
 
 function App() {
   return (
     <div className="App">
+
+      <Menu>
+        <ul>
+          <li><a href="">menu item1</a></li>
+          <li><a href="">menu item2</a></li>
+          <li><a href="">menu item3</a></li>
+        </ul>
+      </Menu>
+
       <Box>
         <StyledBtn as={Link} href={'#'}>LinkComponent</StyledBtn>
         <StyledBtn as='a' href={'#'}>Link</StyledBtn>
         <StyledBtn>hello</StyledBtn>
         <SuperButton>SuperButton</SuperButton>
       </Box>
+
     </div>
   );
 }
@@ -48,12 +58,25 @@ const SuperButton = styled(StyledBtn)`
   background-color: yellow;
 `
 
-const Box = styled.div`
+const Box = styled.div` // wrapper button
  height: 100vh;
  display: flex;
  justify-content: center;
  align-items: center;
  gap: 20px;
+
+ button {
+   cursor: pointer;
+ }
+
+${Link} {
+  cursor: zoom-in;
+}
+`
+const Menu = styled.nav`
+ul {
+  list-style: none;
+}
 `
 
 
